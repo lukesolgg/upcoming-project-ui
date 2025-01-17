2c4872de06f46f625054f997e5bc2aed77729606
lukesolgg <lukesolgg@gmail.com>
2025-01-17 15:32:38 +0000

    added correct metadata, some further styling updates

--- src/app/components/AfterScreen.tsx
+++ src/app/components/AfterScreen.tsx
@@ -5,7 +5,7 @@ import pfp from "../assets/pfp.png"
 const AfterScreen: React.FC = () => {
   return (
     <div className="text-center space-y-4">
-      <p className="text-lg sm:text-xl font-mono mb-4 text-blue-200">The project is not yet ready, please follow LDKCapital on Twitter for further updates.</p>
+      <p className="text-lg sm:text-xl font-mono mb-4 text-blue-200">The project is just a demo, please follow me on Twitter for further updates.</p>
       <Image src={pfp} alt="LDKCapital Profile Picture" width={100} height={100} className="rounded-full mx-auto sm:w-[150px] sm:h-[150px] mb-4" />
       <a href="https://twitter.com/ldkcapital" target="_blank" rel="noopener noreferrer" className="bg-blue-500 font-mono text-white px-4 py-2 sm:px-6 sm:py-3 rounded-full border-2 border-blue-300 hover:bg-blue-600 inline-block">Follow on Twitter</a>
     </div>
--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa'; // Assuming you're using react-icons for the icon
 
 const BackButton: React.FC<{ onBackClick: () => void }> = ({ onBackClick }) => (
-  <button onClick={onBackClick} className="absolute top-4 left-4 flex items-center bg-blue-600 font-mono text-white px-6 py-3 rounded-full border-2 border-blue-300 hover:bg-blue-700">
+  <button onClick={onBackClick} className="absolute top-4 left-4 flex items-center bg-transparent font-mono text-white px-6 py-3 rounded-full border-2 border-blue-300 hover:bg-blue-700">
     <FaArrowLeft className="mr-2" />
     Back
   </button>
