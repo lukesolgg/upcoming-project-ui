61d15623fad511bcfdfc298f123101ce1f678374
lukesolgg <lukesolgg@gmail.com>
2025-01-17 15:40:14 +0000

    added icons to the links section, added mono font to it aswell

--- src/app/components/HeaderLinks.tsx
+++ src/app/components/HeaderLinks.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { FaFileAlt, FaDiscord, FaTwitter } from 'react-icons/fa';
 
 const HeaderLinks: React.FC = () => (
   <div className="absolute top-4 right-4 flex space-x-2 sm:space-x-4">
-    <a href="#" className="text-blue-200 font-mono text-sm sm:text-base">Whitepaper</a>
-    <a href="#" className="text-blue-200 font-mono text-sm sm:text-base">Discord</a>
-    <a href="#" className="text-blue-200 font-mono text-sm sm:text-base">Twitter</a>
+    <a href="#" className="flex items-center text-blue-200">
+      <FaFileAlt className="text-base sm:text-xl sm:mr-2" />
+      <span className="hidden sm:inline font-mono">Whitepaper</span>
+    </a>
+    <a href="#" className="flex items-center text-blue-200">
+      <FaDiscord className="text-base sm:text-xl sm:mr-2" />
+      <span className="hidden sm:inline font-mono">Discord</span>
+    </a>
+    <a href="#" className="flex items-center text-blue-200">
+      <FaTwitter className="text-base sm:text-xl sm:mr-2" />
+      <span className="hidden sm:inline font-mono">Twitter</span>
+    </a>
   </div>
 );
 
